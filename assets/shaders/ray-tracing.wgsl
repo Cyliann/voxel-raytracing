69428bbcd3efe59a8ac1c5f515cd1e9ff715c038
Cyliann <maksymilian@cych.com>
2023-09-13 23:10:34 +0200

    Move raytracing to compute shaders

---
+++ assets/shaders/ray-tracing.wgsl
@@ -0,0 +1,117 @@
+@group(0) @binding(0) var color_buffer: texture_storage_2d<rgba8unorm, write>;
+@group(1) @binding(0)
+var<uniform> camera: CameraUniform;
+
+struct Ray {
+    origin: vec3<f32>,
+    direction: vec3<f32>,
+}
+
+struct CameraUniform {
+    view_pos: vec4<f32>,
+    view: mat4x4<f32>,
+    proj: mat4x4<f32>
+};
+
+@compute @workgroup_size(8,8,1)
+fn main(@builtin(global_invocation_id) GlobalInvocationID: vec3<u32>) {
+    let screen_pos = vec2<i32>(GlobalInvocationID.xy);
+    let screen_size = textureDimensions(color_buffer);
+    var pixel_color = vec3<f32>(.1, .2, .3);
+    let pixel_coord = (vec2<f32>(screen_pos) / vec2<f32>(screen_size)) * 2. - 1.;
+
+    let targetPoint = camera.proj * vec4<f32>(pixel_coord, -1., 1.);
+    var origin = camera.view_pos.xyz;
+    var direction = (camera.view * vec4<f32>(normalize(targetPoint.xyz / targetPoint.w), 0.)).xyz;
+
+    var ray = Ray(origin, direction);
+    var lightDir = vec3<f32>(1.);
+    let dda = dda(ray);
+    let sphere = intersectSphere(ray, lightDir);
+    //if sphere.w > 0. { pixel_color = sphere.xyz; }
+
+    if dda.w == 1. { pixel_color = dda.xyz; }
+
+    textureStore(color_buffer, screen_pos, vec4<f32>(pixel_color, 1.0));
+}
+
+fn dda(r: Ray) -> vec4<f32> {
+    var direction = normalize(r.direction);
+    if direction.x == 0. { direction.x = 0.001; }
+    if direction.y == 0. { direction.y = 0.001; }
+    if direction.z == 0. { direction.z = 0.001; }
+
+    let raySign = vec3<i32>(sign(direction));
+    let rayPositivity = (1 + raySign) / 2;
+    let rayInverse = 1. / direction;
+
+    var gridCoords = vec3<i32>(r.origin);
+    var withinVoxelCoords = r.origin - vec3<f32>(gridCoords);
+
+    var i = 0;
+    while i < 32 {
+        if getVoxel(gridCoords) {
+            return vec4<f32>(vec3f(abs(gridCoords)) / 100., 1.0);
+        }
+
+        let t = (vec3f(rayPositivity) - withinVoxelCoords) * rayInverse;
+        var minIdx: i32;
+        if t.x < t.y {
+            if t.x < t.z {
+                minIdx = 0;
+            } else { minIdx = 2;}
+        } else {
+            if t.y < t.z {
+                minIdx = 1;
+            } else {
+                minIdx = 2;
+            }
+        }
+
+        gridCoords[minIdx] += raySign[minIdx];
+        withinVoxelCoords += direction * t[minIdx];
+        withinVoxelCoords[minIdx] = 1. - f32(rayPositivity[minIdx]);
+        i++;
+    }
+    return vec4<i32>(0.);
+}
+
+fn getVoxel(c: vec3<i32>) -> bool {
+    //return abs(c.x) == abs(c.y) && abs(c.y) == abs(c.z);
+    //return c.x <= 10 && c.x >= 0 && c.y <= 10 && c.y >= 0 && c.z <= 10 && c.z >= 0;
+    return c.z == c.y * c.x;
+}
+
+fn intersectSphere(ray: Ray, lightDir: vec3<f32>) -> vec4<f32> {
+    // (bx^2 + by^2)t^2 + (2(axbx + ayby))t + (ax^2 + ay^2 - r^2) = 0;
+    // where
+    // a -> ray origin
+    // b -> ray direction
+    // t -> ray distance
+    let radius = 1.5;
+    let origin = ray.origin - vec3<f32>(2., 3., 2.);
+
+    let a = dot(ray.direction, ray.direction);
+    let b = 2. * dot(origin, ray.direction);
+    let c = dot(origin, origin) - radius * radius;
+
+    // Delta
+    let delta = b * b - 4. * a * c;
+
+    if delta < 0. {
+        return vec4<f32>(0., 0., 0., 0.);
+    }
+
+    let closestT = (-b - sqrt(delta)) / (2. * a);
+    if closestT <= 0. {return vec4<f32>(0.);}
+
+    let hitPoint = origin + ray.direction * closestT;
+    let normal = normalize(hitPoint);
+    let d = max(dot(normal, lightDir), 0.);
+
+    var sphereColor = vec3<f32>(1., 1., 1.);
+    sphereColor *= d;
+
+    return vec4<f32>(sphereColor, closestT);
+}
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 use nalgebra::*;
 use std::iter;
+use wgpu::BindGroupLayout;
 
 use winit::{
+    dpi::PhysicalSize,
     event::*,
     event_loop::{ControlFlow, EventLoop},
     window::{Window, WindowBuilder},
@@ -46,6 +48,7 @@ struct State {
     config: wgpu::SurfaceConfiguration,
     size: winit::dpi::PhysicalSize<u32>,
     render_pipeline: wgpu::RenderPipeline,
+    render_bind_group: wgpu::BindGroup,
     window: Window,
     camera: camera::Camera,
     camera_controller: camera::CameraController,
@@ -53,6 +56,8 @@ struct State {
     camera_buffer: wgpu::Buffer,
     camera_bind_group: wgpu::BindGroup,
     mouse_pressed: bool,
+    ray_tracing_pipeline: wgpu::ComputePipeline,
+    ray_tracing_bind_group: wgpu::BindGroup,
 }
 
 impl State {
@@ -100,15 +105,13 @@ impl State {
             .unwrap();
 
         let surface_caps = surface.get_capabilities(&adapter);
-        // Shader code in this tutorial assumes an Srgb surface texture. Using a different
-        // one will result all the colors comming out darker. If you want to support non
-        // Srgb surfaces, you'll need to account for that when drawing to the frame.
         let surface_format = surface_caps
             .formats
             .iter()
             .copied()
             .find(|f| f.is_srgb())
             .unwrap_or(surface_caps.formats[0]);
+
         let config = wgpu::SurfaceConfiguration {
             usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
             format: surface_format,
@@ -139,56 +142,17 @@ impl State {
             camera_bind_group_layout,
         ) = create_camera(&device);
 
-        let render_pipeline_layout =
-            device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
-                label: Some("Render Pipeline Layout"),
-                bind_group_layouts: &[&camera_bind_group_layout],
-                push_constant_ranges: &[],
-            });
+        let (ray_tracing_pipeline, ray_tracing_bind_group, sampler, color_buffer_view) =
+            create_raytrace_shader(&device, &size, camera_bind_group_layout);
 
-        let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
-            label: Some("Render Pipeline"),
-            layout: Some(&render_pipeline_layout),
-            vertex: wgpu::VertexState {
-                module: &vert_shader,
-                entry_point: "vs_main",
-                buffers: &[],
-            },
-            fragment: Some(wgpu::FragmentState {
-                module: &frag_shader,
-                entry_point: "fs_main",
-                targets: &[Some(wgpu::ColorTargetState {
-                    format: config.format,
-                    blend: Some(wgpu::BlendState {
-                        color: wgpu::BlendComponent::REPLACE,
-                        alpha: wgpu::BlendComponent::REPLACE,
-                    }),
-                    write_mask: wgpu::ColorWrites::ALL,
-                })],
-            }),
-            primitive: wgpu::PrimitiveState {
-                topology: wgpu::PrimitiveTopology::TriangleList,
-                strip_index_format: None,
-                front_face: wgpu::FrontFace::Ccw,
-                cull_mode: Some(wgpu::Face::Back),
-                // Setting this to anything other than Fill requires Features::POLYGON_MODE_LINE
-                // or Features::POLYGON_MODE_POINT
-                polygon_mode: wgpu::PolygonMode::Fill,
-                // Requires Features::DEPTH_CLIP_CONTROL
-                unclipped_depth: false,
-                // Requires Features::CONSERVATIVE_RASTERIZATION
-                conservative: false,
-            },
-            depth_stencil: None,
-            multisample: wgpu::MultisampleState {
-                count: 1,
-                mask: !0,
-                alpha_to_coverage_enabled: false,
-            },
-            // If the pipeline will be used with a multiview render pass, this
-            // indicates how many array layers the attachments will have.
-            multiview: None,
-        });
+        let (render_pipeline, render_bind_group) = create_render(
+            &device,
+            vert_shader,
+            frag_shader,
+            &config,
+            sampler,
+            color_buffer_view,
+        );
 
         Self {
             surface,
@@ -197,6 +161,7 @@ impl State {
             size,
             config,
             render_pipeline,
+            render_bind_group,
             window,
             camera,
             camera_uniform,
@@ -204,6 +169,8 @@ impl State {
             camera_bind_group,
             camera_controller,
             mouse_pressed: false,
+            ray_tracing_pipeline,
+            ray_tracing_bind_group,
         }
     }
 
@@ -281,6 +248,16 @@ impl State {
             });
 
         {
+            let mut ray_tracing_pass = encoder.begin_compute_pass(&wgpu::ComputePassDescriptor {
+                label: Some("Ray tracing pass"),
+            });
+
+            ray_tracing_pass.set_pipeline(&self.ray_tracing_pipeline);
+            ray_tracing_pass.set_bind_group(0, &self.ray_tracing_bind_group, &[]);
+            ray_tracing_pass.set_bind_group(1, &self.camera_bind_group, &[]);
+            ray_tracing_pass.dispatch_workgroups(self.size.width / 8, self.size.height / 8, 1);
+        }
+        {
             let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
                 label: Some("Render Pass"),
                 color_attachments: &[Some(wgpu::RenderPassColorAttachment {
@@ -302,7 +279,7 @@ impl State {
             // Pipeline
             render_pass.set_pipeline(&self.render_pipeline);
             // Camera
-            render_pass.set_bind_group(0, &self.camera_bind_group, &[]);
+            render_pass.set_bind_group(0, &self.render_bind_group, &[]);
             // Draw
             render_pass.draw(0..3, 0..1);
         }
@@ -312,6 +289,244 @@ impl State {
 
         Ok(())
     }
+}
+
+fn create_camera(
+    device: &wgpu::Device,
+) -> (
+    camera::Camera,
+    camera::CameraController,
+    CameraUniform,
+    wgpu::Buffer,
+    wgpu::BindGroup,
+    wgpu::BindGroupLayout,
+) {
+    let camera = camera::Camera::new(Vector3::new(0.0, 2.0, -12.0), 45., 1., 100.);
+    let camera_controller = camera::CameraController::new(4.0, 1.0);
+
+    let camera_uniform = CameraUniform::new();
+
+    let camera_buffer = wgpu::util::DeviceExt::create_buffer_init(
+        device,
+        &wgpu::util::BufferInitDescriptor {
+            label: Some("Camera Buffer"),
+            contents: bytemuck::cast_slice(&[camera_uniform]),
+            usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
+        },
+    );
+
+    let camera_bind_group_layout =
+        device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+            entries: &[wgpu::BindGroupLayoutEntry {
+                binding: 0,
+                visibility: wgpu::ShaderStages::COMPUTE,
+                ty: wgpu::BindingType::Buffer {
+                    ty: wgpu::BufferBindingType::Uniform,
+                    has_dynamic_offset: false,
+                    min_binding_size: None,
+                },
+                count: None,
+            }],
+            label: Some("camera_bind_group_layout"),
+        });
+
+    let camera_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+        layout: &camera_bind_group_layout,
+        entries: &[wgpu::BindGroupEntry {
+            binding: 0,
+            resource: camera_buffer.as_entire_binding(),
+        }],
+        label: Some("camera_bind_group"),
+    });
+
+    return (
+        camera,
+        camera_controller,
+        camera_uniform,
+        camera_buffer,
+        camera_bind_group,
+        camera_bind_group_layout,
+    );
+}
+
+fn create_raytrace_shader(
+    device: &wgpu::Device,
+    size: &PhysicalSize<u32>,
+    camera_bind_group_layout: BindGroupLayout,
+) -> (
+    wgpu::ComputePipeline,
+    wgpu::BindGroup,
+    wgpu::Sampler,
+    wgpu::TextureView,
+) {
+    let raytrace_shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
+        label: Some("Ray tracing shader"),
+        source: wgpu::ShaderSource::Wgsl(include_str!("../assets/shaders/ray-tracing.wgsl").into()),
+    });
+
+    let color_buffer = device.create_texture(&wgpu::TextureDescriptor {
+        size: wgpu::Extent3d {
+            width: size.width,
+            height: size.height,
+            depth_or_array_layers: 1,
+        },
+        format: wgpu::TextureFormat::Rgba8Unorm,
+        usage: wgpu::TextureUsages::COPY_DST
+            | wgpu::TextureUsages::STORAGE_BINDING
+            | wgpu::TextureUsages::TEXTURE_BINDING,
+        label: Some("Color buffer texture"),
+        mip_level_count: 1,
+        sample_count: 1,
+        dimension: wgpu::TextureDimension::D2,
+        view_formats: &[],
+    });
+
+    let color_buffer_view = color_buffer.create_view(&wgpu::TextureViewDescriptor::default());
+
+    let color_buffer_sampler = device.create_sampler(&wgpu::SamplerDescriptor::default());
+
+    let raytrace_bind_group_layout =
+        device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+            entries: &[wgpu::BindGroupLayoutEntry {
+                binding: 0,
+                visibility: wgpu::ShaderStages::COMPUTE,
+                ty: wgpu::BindingType::StorageTexture {
+                    access: wgpu::StorageTextureAccess::WriteOnly,
+                    format: wgpu::TextureFormat::Rgba8Unorm,
+                    view_dimension: wgpu::TextureViewDimension::D2,
+                },
+                count: None,
+            }],
+            label: Some("color buffer bind group layout"),
+        });
+
+    let raytrace_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+        label: Some("Ray tracing bind group"),
+        layout: &raytrace_bind_group_layout,
+        entries: &[wgpu::BindGroupEntry {
+            binding: 0,
+            resource: wgpu::BindingResource::TextureView(&color_buffer_view),
+        }],
+    });
+
+    let raytrace_pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+        label: Some("Ray tracing Pipeline Layout"),
+        bind_group_layouts: &[&raytrace_bind_group_layout, &camera_bind_group_layout],
+        push_constant_ranges: &[],
+    });
+
+    let raytrace_pipeline = device.create_compute_pipeline(&wgpu::ComputePipelineDescriptor {
+        label: Some("Ray tracing pipeline"),
+        layout: Some(&raytrace_pipeline_layout),
+        module: &raytrace_shader,
+        entry_point: "main",
+    });
+
+    return (
+        raytrace_pipeline,
+        raytrace_bind_group,
+        color_buffer_sampler,
+        color_buffer_view,
+    );
+}
+
+fn create_render(
+    device: &wgpu::Device,
+    vert_shader: wgpu::ShaderModule,
+    frag_shader: wgpu::ShaderModule,
+    config: &wgpu::SurfaceConfiguration,
+    sampler: wgpu::Sampler,
+    color_buffer_view: wgpu::TextureView,
+) -> (wgpu::RenderPipeline, wgpu::BindGroup) {
+    let render_bind_group_layout =
+        device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+            label: Some("Render bind group layout"),
+            entries: &[
+                wgpu::BindGroupLayoutEntry {
+                    binding: 0,
+                    visibility: wgpu::ShaderStages::FRAGMENT,
+                    ty: wgpu::BindingType::Sampler(wgpu::SamplerBindingType::NonFiltering),
+                    count: None,
+                },
+                wgpu::BindGroupLayoutEntry {
+                    binding: 1,
+                    visibility: wgpu::ShaderStages::FRAGMENT,
+                    ty: wgpu::BindingType::Texture {
+                        sample_type: wgpu::TextureSampleType::Float { filterable: false },
+                        view_dimension: wgpu::TextureViewDimension::D2,
+                        multisampled: false,
+                    },
+                    count: None,
+                },
+            ],
+        });
+
+    let render_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+        label: Some("Render bind group"),
+        layout: &render_bind_group_layout,
+        entries: &[
+            wgpu::BindGroupEntry {
+                binding: 0,
+                resource: wgpu::BindingResource::Sampler(&sampler),
+            },
+            wgpu::BindGroupEntry {
+                binding: 1,
+                resource: wgpu::BindingResource::TextureView(&color_buffer_view),
+            },
+        ],
+    });
+
+    let render_pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+        label: Some("Render Pipeline Layout"),
+        bind_group_layouts: &[&render_bind_group_layout],
+        push_constant_ranges: &[],
+    });
+
+    let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+        label: Some("Render Pipeline"),
+        layout: Some(&render_pipeline_layout),
+        vertex: wgpu::VertexState {
+            module: &vert_shader,
+            entry_point: "vs_main",
+            buffers: &[],
+        },
+        fragment: Some(wgpu::FragmentState {
+            module: &frag_shader,
+            entry_point: "fs_main",
+            targets: &[Some(wgpu::ColorTargetState {
+                format: config.format,
+                blend: Some(wgpu::BlendState {
+                    color: wgpu::BlendComponent::REPLACE,
+                    alpha: wgpu::BlendComponent::REPLACE,
+                }),
+                write_mask: wgpu::ColorWrites::ALL,
+            })],
+        }),
+        primitive: wgpu::PrimitiveState {
+            topology: wgpu::PrimitiveTopology::TriangleList,
+            strip_index_format: None,
+            front_face: wgpu::FrontFace::Ccw,
+            cull_mode: Some(wgpu::Face::Back),
+            // Setting this to anything other than Fill requires Features::POLYGON_MODE_LINE
+            // or Features::POLYGON_MODE_POINT
+            polygon_mode: wgpu::PolygonMode::Fill,
+            // Requires Features::DEPTH_CLIP_CONTROL
+            unclipped_depth: false,
+            // Requires Features::CONSERVATIVE_RASTERIZATION
+            conservative: false,
+        },
+        depth_stencil: None,
+        multisample: wgpu::MultisampleState {
+            count: 1,
+            mask: !0,
+            alpha_to_coverage_enabled: false,
+        },
+        // If the pipeline will be used with a multiview render pass, this
+        // indicates how many array layers the attachments will have.
+        multiview: None,
+    });
+
+    (render_pipeline, render_bind_group)
 }
 
 #[cfg_attr(target_arch = "wasm32", wasm_bindgen(start))]
@@ -391,6 +606,7 @@ pub async fn run() {
             Event::RedrawRequested(window_id) if window_id == state.window().id() => {
                 let now = instant::Instant::now();
                 let dt = now - last_render_time;
+                // println!("{:#?}", dt);
                 last_render_time = now;
                 state.update(dt);
                 match state.render() {
@@ -408,62 +624,4 @@ pub async fn run() {
             _ => {}
         }
     });
-}
-
-fn create_camera(
-    device: &wgpu::Device,
-) -> (
-    camera::Camera,
-    camera::CameraController,
-    CameraUniform,
-    wgpu::Buffer,
-    wgpu::BindGroup,
-    wgpu::BindGroupLayout,
-) {
-    let camera = camera::Camera::new(Vector3::new(0.0, 2.0, -12.0), 45., 1., 100.);
-    let camera_controller = camera::CameraController::new(4.0, 1.0);
-
-    let camera_uniform = CameraUniform::new();
-
-    let camera_buffer = wgpu::util::DeviceExt::create_buffer_init(
-        device,
-        &wgpu::util::BufferInitDescriptor {
-            label: Some("Camera Buffer"),
-            contents: bytemuck::cast_slice(&[camera_uniform]),
-            usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
-        },
-    );
-
-    let camera_bind_group_layout =
-        device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
-            entries: &[wgpu::BindGroupLayoutEntry {
-                binding: 1,
-                visibility: wgpu::ShaderStages::FRAGMENT,
-                ty: wgpu::BindingType::Buffer {
-                    ty: wgpu::BufferBindingType::Uniform,
-                    has_dynamic_offset: false,
-                    min_binding_size: None,
-                },
-                count: None,
-            }],
-            label: Some("camera_bind_group_layout"),
-        });
-
-    let camera_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
-        layout: &camera_bind_group_layout,
-        entries: &[wgpu::BindGroupEntry {
-            binding: 1,
-            resource: camera_buffer.as_entire_binding(),
-        }],
-        label: Some("camera_bind_group"),
-    });
-
-    return (
-        camera,
-        camera_controller,
-        camera_uniform,
-        camera_buffer,
-        camera_bind_group,
-        camera_bind_group_layout,
-    );
 }
